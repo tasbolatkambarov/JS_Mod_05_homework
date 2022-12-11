b3a283c52641981e4a6eaf018a4f9794a8a824cc
Tasbolat Kambarov <80477730+tasbolatkambarov@users.noreply.github.com>
2022-12-11 16:47:06 +0600

    Update Mod_05_05.js

--- Mod_05_05.js
+++ Mod_05_05.js
@@ -2,8 +2,9 @@
 
 //Дан произвольный массив. Необходимо вывести количество элементов массива, затем перебрать его и вывести в консоль каждый элемент массива.
 
-let arr = ["I", "learn", "JS", true, 6];
+const arr = ['string', true, NaN, 7, undefined, null, false];
 console.log(arr.length);
-for (let i = 0; i < arr.length; i++) {
+
+for(let i = 0; i < arr.length; i++){
   console.log(arr[i]);
 }
